56a836834aff14fc3c3ca279c8d13694877c8412
Papu-RTL-Work <87756632+Papu-RTL-Work@users.noreply.github.com>
2021-07-25 10:58:15 +0530

    Update i2c_slave_controller.v

--- i2c_slave_controller.v
+++ i2c_slave_controller.v
@@ -5,19 +5,19 @@ module i2c_slave_controller(
 	input i2c_rst, // global reset signal
 	input i2c_scl, // serial clock line
 	inout i2c_sda  // serial data line
-					);
+		    );
 
-	wire 			wr_en_out_in   ; // i2c_slave wr_en_out to ram wr_en 
-	wire        rd_en_out_in	; // i2c_slave rd_en_out to ram rd_en 
+	wire 	    wr_en_out_in   ; // i2c_slave wr_en_out to ram wr_en 
+	wire        rd_en_out_in   ; // i2c_slave rd_en_out to ram rd_en 
 	wire [7:0]  addr_i2c_to_ram; // i2c_slave addr_out to ram addr_in 
 	wire [7:0]  data_i2c_to_ram; // i2c_slave data_out to ram data_in
 	wire [7:0]  data_ram_to_i2c; // ram data_out to i2c_slave data_in
 	
 	// Instatiation of i2c_slave_fsm
 	i2c_slave_fsm i2c_slave_fsm_inst
-	(.rst_in			    (i2c_rst)        , // rst port
-    .i2c_scl	       (i2c_scl)        , // scl port
-    .i2c_sda          (i2c_sda)        , // sda port
+	(.rst_in	   (i2c_rst)        , // rst port
+         .i2c_scl	   (i2c_scl)        , // scl port
+         .i2c_sda          (i2c_sda)        , // sda port
 	 .data_in_from_ram (data_ram_to_i2c), // data_in port from ram
 	 .wr_en_to_ram     (wr_en_out_in)   , // write enable port
 	 .rd_en_to_ram     (rd_en_out_in)   , // read enable port
